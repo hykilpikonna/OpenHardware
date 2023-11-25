64e92375bd6248d782d3bc36a71633b0359887f0
Azalea <me@hydev.org>
2023-11-25 10:34:44 -0800

    [-] Remove path

--- CardReader/start.bat
+++ CardReader/start.bat
@@ -1,6 +1,5 @@
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
-cd Code\CardReader
 :Start
 python receiver.py
 echo Press Ctrl-C if you don't want to restart automatically
