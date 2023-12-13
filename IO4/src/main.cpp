762bf28c93b000e707523ec74eb7464243a6716e
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-12-13 05:49:13 -0500

    [+] Keys

--- IO4/src/main.cpp
+++ IO4/src/main.cpp
@@ -1,8 +1,41 @@
 #include <Arduino.h>
-void setup() {
-// put your setup code here, to run once:
+#include <USBHIDKeyboard.h>
+#include "sweets.h"
+
+#define KEY_COUNT 5
+
+USBHIDKeyboard keyboard;
+constexpr u8 keys[] = {'1', '2', '3', 'a', 'b'};
+constexpr u8 pins[] = {GPIO_NUM_3, GPIO_NUM_4, GPIO_NUM_5, GPIO_NUM_6, GPIO_NUM_7};
+bool keyStates[KEY_COUNT] = {false};
+
+val led_pin = GPIO_NUM_15;
+
+void setup()
+{
+    // Set up the GPIO pins
+    for (val pin : pins) pinMode(pin, INPUT_PULLUP);
+    pinMode(led_pin, OUTPUT);
+
+    // Set up the USB keyboard
+    keyboard.begin();
+    Serial.begin();
 }
 
-void loop() {
-// write your code here
+void loop()
+{
+    // Check for key presses
+    for (var i = 0; i < KEY_COUNT; i++)
+    {
+        val state = !digitalRead(pins[i]);
+
+        if (state != keyStates[i])
+        {
+            if (state) keyboard.press(keys[i]);
+            else keyboard.release(keys[i]);
+
+            digitalWrite(led_pin, state);
+            keyStates[i] = state;
+        }
+    }
 }
