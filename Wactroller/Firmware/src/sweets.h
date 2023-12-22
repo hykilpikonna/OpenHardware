967fe864cb1d2e693724621d022c74d58861e63b
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2023-12-22 02:25:29 -0500

    [+] Wactroller Firmware

---
+++ Wactroller/Firmware/src/sweets.h
@@ -0,0 +1,15 @@
+//
+// Created by Azalea on 12/12/23.
+//
+
+#ifndef SWEETS_H
+#define SWEETS_H
+
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+
+#define val const auto
+#define var auto
+
+#endif //SWEETS_H
